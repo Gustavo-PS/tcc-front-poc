58c367b8a50305873467799658b9c7616a07ae16
rodkawaura <rkmontemor@gmail.com>
2023-05-31 20:43:11 -0300

    Tela de detalhes do aparelho

--- poc_react_IA/src/components/AR.jsx
+++ poc_react_IA/src/components/AR.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Canvas, extend, useThree, useFrame } from 'react-three-fiber';
-import QrScanner from 'react-qr-scanner';
+
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+import './AR.css'
 
 extend({ OrbitControls });
 
@@ -21,15 +23,8 @@ function Controls() {
 
 
 const AR = () => {
-    const [result, setResult] = useState('');
     const [gltf, setGltf] = useState(null);
     const loader = useRef(new GLTFLoader());
-
-    const handleScan = (data) => {
-        if (data) {
-            setResult(data);
-        }
-    };
 
     useEffect(() => {
         loader.current.load('/Phone.glb', setGltf, undefined, console.error);
@@ -38,17 +33,13 @@ const AR = () => {
     const [setIsFrontCamera] = useState(false);
 
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-
-            <div style={{ position: 'relative' }}>
-                <Canvas style={{ width: '800px', height: '1000px' }}>
-                    <ambientLight />
-                    <pointLight position={[10, 10, 10]} />
-                    <Controls />
-                    {gltf && <Model gltf={gltf} />}
-                </Canvas>
-            </div>
-
+        <div className='model'>
+            <Canvas >
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />
+                <Controls />
+                {gltf && <Model gltf={gltf} />}
+            </Canvas>
         </div>
     );
 }
--- poc_react_IA/src/components/ProductInfo.jsx
+++ poc_react_IA/src/components/ProductInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import './ProductInfo.css'
+import AR from './AR'
 
 const ProductInfo = () => {
     const { id } = useParams()
@@ -8,17 +9,49 @@ const ProductInfo = () => {
     const jsonProduct = JSON.parse(id)
 
     return (
-        <div className='device_info'>
-            <a>{jsonProduct.Nome}</a>
-            <a>{jsonProduct.Marca}</a>
-            <a>{jsonProduct["Tamanho da Tela"]}</a>
-            <a>{jsonProduct["Resolução da Tela"]}</a>
-            <a>{jsonProduct["Dimensões do Aparelho"]}</a>
-            <a>{jsonProduct["Memória RAM"]}</a>
-            <a>{jsonProduct.Armazenamento}</a>
-            <a>{jsonProduct.Processador}</a>
-            <a>{jsonProduct.Câmera}</a>
-        </div>
+        <div className='productContainer'>
+            <div className='modelSample'>
+                <AR></AR>
+            </div>
+            <div className='device_info'>
+                <div className='spec'>
+                    <a>Modelo:</a>
+                    <a>{jsonProduct.Nome}</a>
+                </div>
+                <div className='spec'>
+                    <a>Marca:</a>
+                    <a>{jsonProduct.Marca}</a>
+                </div>
+                <div className='spec'>
+                    <a>Tamanho da tela:</a>
+                    <a> {jsonProduct["Tamanho da Tela"]}</a>
+                </div>
+                <div className='spec'>
+                    <a>Resolução da tela:</a>
+                    <a>{jsonProduct["Resolução da Tela"]}</a>
+                </div>
+                <div className='spec'>
+                    <a>Dimensões:</a>
+                    <a>{jsonProduct["Dimensões do Aparelho"]}</a>
+                </div>
+                <div className='spec'>
+                    <a>Memória RAM:</a>
+                    <a>{jsonProduct["Memória RAM"]}</a>
+                </div>
+                <div className='spec'>
+                    <a>Armazenamento:</a>
+                    <a>{jsonProduct.Armazenamento}</a>
+                </div>
+                <div className='spec'>
+                    <a>Processador:</a>
+                    <a>{jsonProduct.Processador}</a>
+                </div>
+                <div className='spec'>
+                    <a>Câmera</a>
+                    <a>{jsonProduct.Câmera}</a>
+                </div>
+            </div>
+        </div >
     )
 }
 
