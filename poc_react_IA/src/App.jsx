e4943091aa896df1b1a69ab49d80a7bc3f453ee0
rodkawaura <rkmontemor@gmail.com>
2023-10-04 20:48:05 -0300

    correção gramatical no quiz, tratativa de exceções

--- poc_react_IA/src/App.jsx
+++ poc_react_IA/src/App.jsx
@@ -12,18 +12,22 @@ import MenuQuiz from "./components/MenuQuiz";
 
 import Navbar from "./components/Navbar";
 import { Outlet } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
+import NotFound from './components/NotFound.jsx'
 
 function App() {
 
   const [quizState, dispatch] = useContext(QuizContext)
   const [token, setToken] = useState([])
 
-    return (
+  return (
+    <ErrorBoundary FallbackComponent={NotFound}>
       <div className="App">
         <Outlet></Outlet>
       </div>
-    )
-  }
+    </ErrorBoundary>
+  )
+}
 
 
 export default App
